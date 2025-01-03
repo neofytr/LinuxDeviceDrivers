f3bc511be2051b55c22fd35d71de6e303340e8ca
RajDaBest <imagination12344321.com@gmail.com>
2025-01-04 00:39:10 +0530

    binded a read function

--- neo.c
+++ neo.c
@@ -10,8 +10,18 @@ MODULE_DESCRIPTION("Basic read loadable kernel module");
 static struct proc_dir_entry *custom_proc_node;
 static struct proc_ops neo_proc_ops;
 
+static ssize_t neo_read(struct file *file_ptr, char __user *user_space_buffer, size_t count, loff_t *offset);
+
+static ssize_t neo_read(struct file *file_ptr, char __user *user_space_buffer, size_t count, loff_t *offset)
+{
+    printk("neo_read\n");
+    return 0;
+}
+
 static int neo_module_init(void)
 {
+    neo_proc_ops.proc_read = neo_read; // binding neo_read function to read on /proc/neo
+
     // NULL in the 3rd argument creates the new file in the root of the proc directory
     custom_proc_node = proc_create("neo", S_IRUSR | S_IRGRP | S_IROTH, NULL, &neo_proc_ops);
     if (!custom_proc_node)
