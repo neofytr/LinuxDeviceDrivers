82a33ef3c577648601271668ed1046dc204c2fb3
RajDaBest <imagination12344321.com@gmail.com>
2025-01-03 23:53:21 +0530

    module updated

--- neo.c
+++ neo.c
@@ -1,19 +1,25 @@
 #include <linux/module.h>
+#include <linux/stat.h>
 #include <linux/init.h>
+#include <linux/proc_fs.h>
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("neofyt");
-MODULE_DESCRIPTION("My first dynamically loadable kernel module neo");
+MODULE_AUTHOR("neofyt (rajisbornforcoc@gmail.com)");
+MODULE_DESCRIPTION("Basic read loadable kernel module");
+
+static struct proc_dir_entry *custom_proc_node;
+static struct proc_ops neo_proc_ops;
 
 static int neo_module_init(void)
 {
-    printk("Hello, World! (from neo)\n");
+    // NULL in the 3rd argument creates the new file in the root of the proc directory
+    custom_proc_node = proc_create("neo", S_IRUSR | S_IRGRP | S_IROTH, NULL, &neo_proc_ops);
     return 0;
 }
 
 static void neo_module_exit(void)
 {
-    printk("Goodbye! (from neo)\n");
+    proc_remove(custom_proc_node);
 }
 
 module_init(neo_module_init);
