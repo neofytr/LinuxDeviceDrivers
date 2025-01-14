c94925cfbea9a5e94be7868d32cc34ea78077642
neofytr <rajisbornforcoc@gmail.com>
2025-01-14 16:40:10 +0530

    module param exporting

--- hello_world/main.c
+++ hello_world/main.c
@@ -1,7 +1,15 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/sched.h>
+#include <linux/stat.h>
 #include <asm/current.h>
+#include <linux/errno.h>
+
+static char *whom = "world";
+static int howmany = 1;
+
+module_param(howmany, int, S_IRUGO);
+module_param(whom, charp, S_IRUGO);
 
 MODULE_LICENSE("GPL");
 
